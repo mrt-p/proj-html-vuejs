41d337da76e1c38dc40aac13748b36fcef1f3695
Marta <martaprudentino@gmail.com>
2021-04-13 16:30:41 +0200

    Header Layout + Vue

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var app = new Vue ({
+    el: "#app",
+    data: {
+        logo: 'img/white-logo-2.png',
+        links: [
+            {
+                name: 'Home'
+            },
+            {
+                name: 'About'
+            },
+            {
+                name: 'Services'
+            },
+            {
+                name: 'Blog'
+            },
+            {
+                name: 'Contact'
+            },
+            {
+                name: 'Portfolio'
+            }
+        ],
+        button: {
+            name: 'Sign In'
+        }
+    },
+    
+    methods: {
+        
+    }
+});
