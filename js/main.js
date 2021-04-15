124d34bcddd136e8ddbcc20025e7d50df0d34a86
Marta <martaprudentino@gmail.com>
2021-04-15 16:56:33 +0200

    Footer

--- js/main.js
+++ js/main.js
@@ -24,7 +24,42 @@ var app = new Vue ({
         ],
         button: {
             name: 'Sign In'
-        }
+        },
+        quicklinks: [
+            {
+                name: 'Services'
+            },
+            {
+                name: 'Contact'
+            },
+            {
+                name: 'Blog'
+            }
+        ],
+        resourceslinks: [
+            {
+                name: 'Art Design'
+            },
+            {
+                name: 'Computer'
+            },
+            {
+                name: 'IT News'
+            },
+            {
+                name: 'Networking'
+            },
+            {
+                name: 'Web Security'
+            }
+        ],
+        contactus: {
+                address: 'Patricia C. Amedee 4401 Waldeck Street Grapevine Nashville, Tx 76051',
+            
+                phone: '+99 (0) 101 0000 888',
+            
+                email: 'Info@yourdomain.com',
+            },
     },
     
     methods: {
